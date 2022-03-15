0ab58a66c0b05b8f9e05d31e01e2f9080fb5f82f
jelavicluka <73234941+jelavicluka@users.noreply.github.com>
2022-03-15 18:27:36 +0100

    Update

---
+++ PIS_vj1/Newclass.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PISLabVj1
+{
+    class NewClass
+    {
+        int metoda()
+        {
+            return 5;
+        }
+    }
+}

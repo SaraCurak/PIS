c515f1525e206d9fc0074b7c3d400fe80f491ead
SaraCurak <saracurak99@gmail.com>
2022-03-15 18:25:27 +0100

    Dodata metoda.

---
+++ PIS_vj1/Newclass.cs
@@ -0,0 +1,15 @@
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PISL_vj1
+{
+    class NewClass
+    {
+        int metoda ()
+        {
+            return 3;
+        }
+    }
+}
